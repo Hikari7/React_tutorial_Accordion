80a1dbae84276410a1cd881b4bc2649443ecf690
Hikari7 <h.kobe712@gmail.com>
2022-08-27 22:03:30 -0700

    done accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
+//Hooks書いていく
 const Accordion = ({ items }) => {
+  //分割代入
+  //activeIndexはpart of state
+  //setActiveIndesはsetActiveindexを呼んでindexのを更新する
+  //useStateでデフォルト値に
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+    //これが呼び出されるとそのindexはprovideされて、const Accordingがre-renderされる (null)に戻る
+  };
+
+  //もしindexがactiveIndexと同じだったらactiveを付与して、そうじゃなかったら空
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div
-          className="title active"
-          onClick={() => console.log("Title clicked", index)}
-        >
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-
-  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
